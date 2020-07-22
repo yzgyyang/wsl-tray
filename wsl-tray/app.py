fff50e6c483284261a1d6a6a3e335a8f9673e996
Guangyuan Yang <yzgyyang@outlook.com>
2020-07-22 03:11:24 -0400

    Update app.py: add menu items

--- wsl-tray/app.py
+++ wsl-tray/app.py
@@ -1,3 +1,7 @@
+from functools import partial
+
+import wsl_operations as wsl
+
 from systray import SysTrayIcon
 from systray.win32_adapter import *
 
@@ -5,12 +9,25 @@ APP = "wsl-tray"
 APP_DISPLAY = "WSL Tray"
 
 
+# Actions
 def say_hello(systray):
     print("Hello, World!")
 
 
+def toggle_wsl_state(distro_name, systray):
+    return wsl.toggle_state(distro_name)
+
+
 def update_menu(systray):
-    pass
+    # Distros
+    info = wsl.get_all_states()
+    menu_options = tuple([(f"[{v['state']}] {name}", None, partial(toggle_wsl_state, name)) for name, v in info.items()])
+
+    # Start/Stop all
+    menu_options += (("Terminate All", None, wsl.shutdown_all),)
+    menu_options += (("Shutdown All + WSL 2 Backend", None, wsl.shutdown_all),)
+
+    systray.update(menu_options=menu_options)
 
 
 def on_quit_callback(systray):
@@ -19,14 +36,14 @@ def on_quit_callback(systray):
 
 NOTIFY_DICT = {
     WM_LBUTTONDBLCLK: say_hello,
+    WM_RBUTTONUP: update_menu,
 }
 
 
 if __name__ == "__main__":
-    menu_options = (("Say Hello", None, say_hello),)
     systray = SysTrayIcon("icon.ico",
                           APP_DISPLAY,
-                          menu_options,
+                          menu_options=tuple(),
                           on_quit=on_quit_callback,
                           notify_dict=NOTIFY_DICT)
     systray.start()
