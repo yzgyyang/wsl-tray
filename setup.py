533086ef8846574250e4482edf94ee0ccbc355ba
Guangyuan Yang <yzgyyang@outlook.com>
2020-07-22 10:53:06 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wsl-tray",
+    version="0.1",
+    author="Guangyuan Yang",
+    author_email="yzgyyang@outlook.com",
+    description="wsl-tray is a lightweight Windows tray application for easily managing WSL 2 VMs.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yzgyyang/wsl-tray",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+    ],
+    python_requires='>=3.7',
+)
