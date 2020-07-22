9388ee89526b348f6ceb1dee8225c8fc4a37fabb
Guangyuan Yang <yzgyyang@outlook.com>
2020-07-22 03:29:39 -0400

    Update app.py: add linebreaks and open about page

--- wsl-tray/app.py
+++ wsl-tray/app.py
@@ -1,3 +1,5 @@
+import subprocess
+
 from functools import partial
 
 import wsl_operations as wsl
@@ -18,20 +20,30 @@ def toggle_wsl_state(distro_name, systray):
     return wsl.toggle_state(distro_name)
 
 
+def open_about(systray):
+    cmd = 'explorer "https://github.com/yzgyyang/wsl-tray"'
+    subprocess.run(cmd)
+
+
+def no_action(systray):
+    pass
+
+
 def update_menu(systray):
     # Distros
     info = wsl.get_all_states()
     menu_options = tuple([(f"[{v['state']}] {name}", None, partial(toggle_wsl_state, name)) for name, v in info.items()])
+    menu_options += (("-----", None, no_action),)
 
     # Start/Stop all
     menu_options += (("Terminate All", None, wsl.shutdown_all),)
     menu_options += (("Shutdown All + WSL 2 Backend", None, wsl.shutdown_all),)
+    menu_options += (("-----", None, no_action),)
+
+    # About
+    menu_options += (("About", None, open_about),)
 
     systray.update(menu_options=menu_options)
-
-
-def on_quit_callback(systray):
-    pass
 
 
 NOTIFY_DICT = {
@@ -44,6 +56,6 @@ if __name__ == "__main__":
     systray = SysTrayIcon("icon.ico",
                           APP_DISPLAY,
                           menu_options=tuple(),
-                          on_quit=on_quit_callback,
+                          on_quit=no_action,
                           notify_dict=NOTIFY_DICT)
     systray.start()
